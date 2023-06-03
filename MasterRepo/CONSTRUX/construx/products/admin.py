21b1db3b71d9d69a287b4cdc612d1922ce3e31d5
ABUDIYAAAA <91617550+ABUDIYAAAA@users.noreply.github.com>
2023-06-03 20:33:29 +0530

    Commit 2
    Commit 2

--- MasterRepo/CONSTRUX/construx/products/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Cart, Project, Items
-# Register your models here.
-admin.site.register(Cart)
-admin.site.register(Project)
-admin.site.register(Items)
